cc4aad8dee718dc7bb7b7a458396cf4aeb1412bb
hchiu <hchiu@3a99199a-f423-4342-a60e-77579ae45dae>
2014-09-23 19:45:53 +0000

    testing my own stuff on m0, part 13

---
+++ m0/hchiu/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form name="wishList" action="wishlist.php">
+            Show wish list of: <input type="text" name="user" value="" />
+            <input type="submit" value="Go" />
+        </form>
+    </body>
+</html>
---
+++ m0/hchiu/wishlist.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>Wish List of <?php echo htmlentities($_GET["user"]) . "<br/>"; ?>
+        
+    </body>
+</html>
