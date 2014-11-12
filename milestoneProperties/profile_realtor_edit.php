dbae4113f28981d9cd9eb5383359e7e4957f301a
jondo <jondo@3a99199a-f423-4342-a60e-77579ae45dae>
2014-11-12 05:53:34 +0000

    profile add-on (getting started)

---
+++ milestoneProperties/profile_realtor_edit.php
@@ -0,0 +1,23 @@
+<?php include 'navbar.php';    ?>
+<?php include_once 'functions.php'; ?>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+        <title>Edit Profile</title>
+        <style>
+            .breadcrumb{
+                background: none;
+                text-align: left;
+            }
+            body{padding-top:4%;}
+        </style>
+    </head>
+    <body>
+        <div class="container text-center">
+            <h1>(%Realtor)'s Profile</h1>                
+        </div>
+    </body>
+</html>
