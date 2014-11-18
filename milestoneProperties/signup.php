87740f752f40670af64268344b7396206afa3d90
jondo <jondo@3a99199a-f423-4342-a60e-77579ae45dae>
2014-11-18 22:32:17 +0000

    Matching the Format for Sign Up

--- milestoneProperties/signup.php
+++ milestoneProperties/signup.php
@@ -6,45 +6,70 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
-        <title>User Profile</title>
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Crimson+Text' rel='stylesheet' type='text/css'>
+        <title>Sign Up</title>
         <style>
             .breadcrumb{
                 background: none;
                 text-align: left;
             }
-            body{padding-top:4%;}
+            body{padding-top:0%;}
+            .top-container{
+                margin-top: 80px;
+/*                background-color:#e5e5e5;*/
+                border-radius: 10px; 
+                
+            }
+            .transbox{
+                background:rgba(0, 0, 0, .07);
+                border-radius: 10px; 
+                box-shadow: 1px 7px 36px -5px;
+            }
         </style>
     </head>
     <body>
-        <div class="container text-center">
-            <h1>Account Creation</h1>                
-        </div
-        
-        <form role="form">
-            <div class="input-group input-group-sm col-sm-offset-1 col-sm-4">
-                <label for="InputEmail">Email</label>
-                <input type="email" class="form-control" id="InputEmail" placeholder="Enter Email">
+        <div class="container top-container transbox">
+            <div class="container text-center">
+                <h1>Account Creation</h1>
             </div>
-            <div class="input-group input-group-sm col-sm-offset-1 col-sm-4">
-                <label for="InputPW1">New Password</label>
-                <input type="password" class="form-control" id="InputPW1" placeholder="Create Password">
-            </div>
-            <div class="input-group input-group-sm col-sm-offset-1 col-sm-4">
-                <label for="InputPW2">Re-Enter New Password</label>
-                <input type="password" class="form-control" id="InputPW2" placeholder="Re-Enter Password">
-            </div>
-            <div class="input-group input-group-sm col-sm-offset-1 col-sm-4">
-                <label for="InputFirstName">First Name</label>
-                <input type="firstname" class="form-control" id="InputFirstName" placeholder="First Name">
-            </div>
-           <div class="input-group input-group-sm col-sm-offset-1 col-sm-4">
-                <label for="InputLastName">Last Name</label>
-                <input type="lastname" class="form-control" id="InputLastName" placeholder="Last Name">
-            </div>
-            <div class="input-group input-group-sm col-sm-offset-1 col-sm-4">
-                <button type="submit" class="btn btn-default">Submit</button>
-            </div>
-        </form>
-        
+            <form role="form">
+                <div class="form-group">
+                    <div class="input-group input-group-sm col-sm-offset-1 col-sm-4">
+                        <label for="InputEmail">Email</label>
+                        <input type="email" class="form-control" id="InputEmail" placeholder="Enter Email">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group input-group-sm col-sm-offset-1 col-sm-4">
+                        <label for="InputPW1">New Password</label>
+                        <input type="password" class="form-control" id="InputPW1" placeholder="Create Password">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group input-group-sm col-sm-offset-1 col-sm-4">
+                        <label for="InputPW2">Re-Enter New Password</label>
+                        <input type="password" class="form-control" id="InputPW2" placeholder="Re-Enter Password">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group input-group-sm col-sm-offset-1 col-sm-4">
+                        <label for="InputFirstName">First Name</label>
+                        <input type="firstname" class="form-control" id="InputFirstName" placeholder="First Name">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group input-group-sm col-sm-offset-1 col-sm-4">
+                        <label for="InputLastName">Last Name</label>
+                        <input type="lastname" class="form-control" id="InputLastName" placeholder="Last Name">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group input-group-sm col-sm-offset-1 col-sm-4">
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div> 
     </body>
 </html>
