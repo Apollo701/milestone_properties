60b3979d03da33667d1945b2b86fac472e2519aa
ssastr <ssastr@3a99199a-f423-4342-a60e-77579ae45dae>
2014-11-19 06:33:45 +0000

    Changes made to About Us Page

--- milestoneProperties/about.php
+++ milestoneProperties/about.php
@@ -7,10 +7,81 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
         <title>About Us Page</title>
+        <style>
+            body {
+                padding-top: 70px; 
+            }
+
+            .img-center {
+                margin: 0 auto;
+            }
+        </style>
     </head>
     <body>
-        
-    
-     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+        <div class="container">
+
+            <!-- Introduction Row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">About Us
+                        <small>It's Nice to Meet You!</small>
+                    </h1>
+                    <p>Real estate web site as a part of SFSU SE course</p>
+                </div>
+            </div>
+
+            <!-- Team Members Row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2 class="page-header">Our Team</h2>
+                </div>
+                <div class="col-lg-4 col-sm-6 text-center">
+                    <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                    <h3>Jason Dorn
+                        <small>Team Lead</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+                <div class="col-lg-4 col-sm-6 text-center">
+                    <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                    <h3>Sam
+                        <small>Tech Lead</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+                <div class="col-lg-4 col-sm-6 text-center">
+                    <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                    <h3>Rachel Beasley
+                        <small>Job Title</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+                <div class="col-lg-4 col-sm-6 text-center">
+                    <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                    <h3>Sree Vidya Sastry
+                        <small>Job Title</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+                <div class="col-lg-4 col-sm-6 text-center">
+                    <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                    <h3>Harry
+                        <small>Job Title</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+                <div class="col-lg-4 col-sm-6 text-center">
+                    <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                    <h3>Jon Do
+                        <small>Job Title</small>
+                    </h3>
+                    <p>Description</p>
+                </div>
+            </div>
+
+
+
+
+            <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     </body>
 </html>
