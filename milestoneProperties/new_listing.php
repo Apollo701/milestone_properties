e089747d2e14b0aa4e2db34120c08170926be2ec
jondo <jondo@3a99199a-f423-4342-a60e-77579ae45dae>
2014-11-20 04:25:55 +0000

    Made Sell Home more attractive

--- milestoneProperties/new_listing.php
+++ milestoneProperties/new_listing.php
@@ -4,24 +4,36 @@
 <!DOCTYPE>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
-        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-        <link href='http://fonts.googleapis.com/css?family=Crimson+Text' rel='stylesheet' type='text/css'>
-        <title>Milestone Properties</title>
+        <?php run_scripts_head()?>
+        <title>Sell Home</title>
         <style>
-            #sellhome{
-            padding-top:4%;
+            .breadcrumb{
+                background: none;
+                text-align: left;
             }
-        .navbar-brand{
+            .navbar-brand{
             font-family: 'Crimson Text', serif;
-
-        }
+            }
+            body{padding-top:0%;}
+            .top-container{
+                margin-top: 80px;
+/*                background-color:#e5e5e5;*/
+                border-radius: 10px; 
+                
+            }
+            .transbox{
+                background:rgba(0, 0, 0, .07);
+                border-radius: 10px; 
+                box-shadow: 1px 7px 36px -5px;
+            }
         </style>
     </head>
     <body>
-        <div class="container" id="sellhome">
+        <?php run_scripts_body()?>
+        <div class="container top-container transbox" id="sellhome">
+            <div class="container text-center">
+                <h1>Sell Home</h1>
+            </div>
         <form action="new_listing_created.php" method="post" enctype="multipart/form-data">
             State:      <input type="text" name="us_state" value="" /><br />
             <br />
