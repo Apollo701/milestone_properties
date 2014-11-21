1cf6d5cb994203c2a01ee4f9f4a7f77aa112c06e
ssastr <ssastr@3a99199a-f423-4342-a60e-77579ae45dae>
2014-11-21 06:23:21 +0000

    Adding a fixed footer to all pages

--- milestoneProperties/footer.php
+++ milestoneProperties/footer.php
@@ -4,5 +4,6 @@
         <p class="navbar-text navbar-left" ><a href="./contact.php" style="color:#777">Contact Us</a></p>
         <p class="navbar-text navbar-left"><a href="./about.php" style="color:#777">About Us</a></p>
         <p class="navbar-text navbar-right" style="margin-right: 15px;">This is for demonstration purposes only. CSC648/848 San Francisco State University Team02 Milestone Properties&copy</p>
+
     </div>
 </div>
