f539e120fb4c5e38cbc19c058562c6e105975e9f
jdorn <jdorn@3a99199a-f423-4342-a60e-77579ae45dae>
2014-12-06 01:05:36 +0000

    fixed part of functions.php that resized image

--- milestoneProperties/functions.php
+++ milestoneProperties/functions.php
@@ -153,8 +153,8 @@ function display_search_results($result) {
             echo '</div>';
             echo '<div class="panel-body">';
             $img_name = $row["image1"];
-            $img_path = 'http://sfsuswe.com/~f14g02/assets/home_images/home' . $row["id"] . '/large/' . $img_name;
-            echo '<img src=" ' . $img_path . '" ' . 'style="width:250px;height:200px" />';
+            $img_path = 'http://sfsuswe.com/~f14g02/assets/home_images/home' . $row["id"] . '/small/' . $img_name;
+            echo '<img src=" ' . $img_path . '" />';
             echo '</div>';
             echo '</div>';
             echo '</div>';
@@ -410,7 +410,7 @@ function display_formatted_results($result) {
                             <a class="pull-left" href="#" target="_parent">';
 
             $img_name = $row["image1"];
-            $img_path = 'http://sfsuswe.com/~f14g02/assets/images/' . $img_name;
+            $img_path = 'http://sfsuswe.com/~f14g02/assets/home_images/home' . $row["id"] . '/small/' . $img_name;
             echo '<img class="img-responsive" src="' . $img_path . '"/></a>
 
                             <div class="clearfix visible-sm"></div>
@@ -486,7 +486,7 @@ function display_formatted_details($result) {
                             <a class="pull-left" href="#" target="_parent">';
 
             $img_name = $row["image1"];
-            $img_path = 'http://sfsuswe.com/~f14g02/assets/images/' . $img_name;
+            $img_path = 'http://sfsuswe.com/~f14g02/assets/home_images/home' . $row["id"] . "/small/" . $img_name;
             echo '<img class="img-responsive" src=" ' . $img_path . '" /></a>
 
                             <div class="clearfix visible-sm"></div>
