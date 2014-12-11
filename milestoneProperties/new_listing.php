4ba2a41f16d8543d51e8e3a5ae0c12330178be20
jdorn <jdorn@3a99199a-f423-4342-a60e-77579ae45dae>
2014-12-11 04:48:39 +0000

    included new javascript library for uploading files

--- milestoneProperties/new_listing.php
+++ milestoneProperties/new_listing.php
@@ -70,5 +70,8 @@
         <p class="navbar-text navbar-right" style="margin-right: 15px;">This is for demonstration purposes only. CSC648/848 San Francisco State University Team02 Milestone Properties&copy</p>
 
     </div>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+        <script src="./path/to/dropzone.js"></script>
     </body>
 </html>
