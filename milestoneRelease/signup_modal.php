17bbb31ec7d3d1b07fae45b9ab5f19a3f529c735
jdorn <jdorn@3a99199a-f423-4342-a60e-77579ae45dae>
2014-12-12 07:54:10 +0000

    working condition . removed username from signup modal

--- milestoneRelease/signup_modal.php
+++ milestoneRelease/signup_modal.php
@@ -12,10 +12,7 @@
 <!-- register form -->
 <form method="post" action="FILL ME IN" name="registerform">
 
-    <!-- the user name input field uses a HTML5 pattern check -->
-    <label for="login_input_username">Username (only letters and numbers, 2 to 64 characters)</label>
-    <input id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required />
-    <br>
+    
     <!-- the email input field uses a HTML5 email type check -->
     <label for="login_input_email">User's email</label>
     <input id="login_input_email" class="login_input" type="email" name="user_email" required />
