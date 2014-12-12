9a292731c7d12a2e455e1fab9d69874971d6ca83
hchiu <hchiu@3a99199a-f423-4342-a60e-77579ae45dae>
2014-12-12 06:19:22 +0000

    fiddling around with login_forgot.php 's ui

--- milestoneRelease/login_forgot.php
+++ milestoneRelease/login_forgot.php
@@ -1,6 +1,6 @@
-<?php include 'navbar.php';    ?>
-<?php include_once 'functions.php'; ?>
-<?php include 'footer.php'; ?>
+<?php include 'navbar.php';
+      include_once 'functions.php';
+      //include 'footer.php';?>
 
 <html lang="en">
     <head>
@@ -16,14 +16,14 @@
                 text-align: left;
             }
             .navbar-brand{
-            font-family: 'Crimson Text', serif;
+                font-family: 'Crimson Text', serif;
             }
             body{padding-top:0%;}
             .top-container{
                 margin-top: 80px;
-/*                background-color:#e5e5e5;*/
+                /*                background-color:#e5e5e5;*/
                 border-radius: 10px; 
-                
+
             }
             .transbox{
                 background:rgba(0, 0, 0, .07);
@@ -34,16 +34,41 @@
     </head>
     <body>
         <?php run_scripts_body();
-            if(!empty($_POST)) {
-                check_login();
-            }
+            static $emptyFields;
+            static $emailNotValid;
         ?>
-        
+
         <div class="container top-container transbox">
             <div class="container text-center">
                 <h1>Account Recovery</h1>
             </div>
-       
+            
+            <form role="form" method="post">
+                <div class="form-group">
+                    <div class="input-group input-group-sm col-sm-offset-4 col-sm-4">
+                        <label for="InputEmail">Email</label>
+                        <input type="email" class="form-control" name="InputEmail" placeholder="Enter Email" value="<?php echo isset($_POST['InputEmail']) ? $_POST['InputEmail'] : '' ?>">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group input-group-sm col-sm-offset-4 col-sm-4">
+                        <button type="submit" class="btn btn-default">Recover Password</button>
+                        <span class="error"> <?php echo display_errors() ?></span>
+                    </div>
+                </div>
+            </form>
         </div> 
     </body>
 </html>
+
+<?php
+    function display_errors() {
+        if($GLOBALS['emptyFields']) {
+            echo "Fields cannot be empty";
+        }
+        
+        else if($GLOBALS['emailNotValid']) {
+            echo "Email is not valid";
+        }
+    }
+?>
