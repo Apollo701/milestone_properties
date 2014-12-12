c736a4940a5b94035426eba79cb1bfe0e94a44b7
jdorn <jdorn@3a99199a-f423-4342-a60e-77579ae45dae>
2014-12-12 02:06:38 +0000

    organized functions related to login, into an includes folder, and added logout.php, functions.php (login only) and process_login.php

--- milestoneRelease/includes/db_connect.php
+++ milestoneRelease/includes/db_connect.php
@@ -1,9 +1,3 @@
 <?php
-include_once 'msp-config.php';
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+include_once 'msp-config.php';   // As functions.php is not included
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
