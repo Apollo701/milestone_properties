6e4359010ad06bb7759be60b9782ba1899eba501
hchiu <hchiu@3a99199a-f423-4342-a60e-77579ae45dae>
2014-12-15 08:02:03 +0000

    added footer to about.php

--- milestoneDevelopment/about.php
+++ milestoneDevelopment/about.php
@@ -1,6 +1,6 @@
 <?php include 'navbar.php'; ?>
 <?php include_once 'functions.php'; ?>
-<?php include 'footer.php'; ?>
+<?php// include 'footer.php'; ?>
 <!doctype html>
 <html lang="en">
     <head>
@@ -88,10 +88,15 @@
             </div>
         </div>
         <br><br><br><br><br>
+        
+        <div class="container container-fluid" style="background-color: #e7e7e7; border-color: #777; width: 100%; position: absolute;left: 0;right: 0">
+            <p class="navbar-text navbar-left" ><a href="./contact.php" style="color:#777">Contact Us</a></p>
+            <p class="navbar-text navbar-left"><a href="./about.php" style="color:#777">About Us</a></p>
+            <p class="navbar-text navbar-right" style="margin-right: 15px;">This is for demonstration purposes only. CSC648/848 San Francisco State University Team02 Milestone Properties&copy</p>
+        </div>
 
 
 
-
-            <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     </body>
 </html>
